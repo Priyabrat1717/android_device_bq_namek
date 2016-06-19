0943f2705bf39decd08ca3575783d27538cfb1f5
Kra1o5 <kra1o5x@gmail.com>
2016-06-19 19:50:19 +0200

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,22 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common CM stuff
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/bq/namek/full_namek.mk)
+
+PRODUCT_RELEASE_NAME := Aquaris M5.5
+PRODUCT_NAME := cm_namek
+
+# Set product device & name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   TARGET_DEVICE=Aquaris_M55 PRODUCT_NAME=Aquaris_M55
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=bq/Aquaris_M55/Aquaris_M55:6.0.1/MMB29M/1463134633:user/release-keys \
+    PRIVATE_BUILD_DESC="Aquaris_M55_msm8916_32-user 6.0.1 MMB29M 493 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-bq
