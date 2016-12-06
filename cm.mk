226a93eeafafe67c1d90b5a9508f5e2b98c7b7fe
Kra1o5 <kra1o5x@gmail.com>
2016-12-07 00:18:49 +0100

    Update fingerprint from 2.2.1 release

--- cm.mk
+++ cm.mk
@@ -16,7 +16,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
    TARGET_DEVICE=Aquaris_M55 PRODUCT_NAME=Aquaris_M55
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=bq/Aquaris_M55/Aquaris_M55:6.0.1/MMB29M/1468313303:user/release-keys \
-    PRIVATE_BUILD_DESC="Aquaris_M55_msm8916_32-user 6.0.1 MMB29M 597 release-keys"
+    BUILD_FINGERPRINT=bq/Aquaris_M55/Aquaris_M55:6.0.1/MMB29M/1474387552:user/release-keys \
+    PRIVATE_BUILD_DESC="Aquaris_M55_msm8916_32-user 6.0.1 MMB29M 670 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-bq
